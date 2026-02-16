eda2a68ea2e4d2643211b7d2d82ab54c92d299aa
vishakhviswan <vishakhviswan1995@gmail.com>
2026-02-16 23:18:47 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/offline-catalog-app/sw.js', { scope: '/offline-catalog-app/' })})}
